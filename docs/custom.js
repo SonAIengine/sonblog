d9dd53f8a8a9172598e8764225654ce8e5ac4308
sonsj97 <sonsj97@naver.com>
2024-10-11 09:38:59 +0900

    add: 글 추가

---
+++ docs/custom.js
@@ -0,0 +1,7 @@
+// custom.js
+document.addEventListener("DOMContentLoaded", function() {
+  // 해당 span 요소 선택 및 클릭 시 링크로 이동
+  document.querySelector('.md-ellipsis').addEventListener('click', function() {
+    window.location.href = 'https://sonaiengine.github.io/sonblog/';
+  });
+});

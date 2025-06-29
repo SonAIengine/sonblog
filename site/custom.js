6aca411a530875cc235d0e935a089bbda9833560
sonsj97 <166786347+SonAIengine@users.noreply.github.com>
2025-06-29 23:48:25 +0900

    Delete site directory

--- site/custom.js
+++
@@ -1,7 +0,0 @@
-// custom.js
-document.addEventListener("DOMContentLoaded", function() {
-  // 해당 span 요소 선택 및 클릭 시 링크로 이동
-  document.querySelector('.md-ellipsis').addEventListener('click', function() {
-    window.location.href = 'https://sonaiengine.github.io/sonblog/';
-  });
-});
